126220070b6d0ef1b9a97b42413ae1c5cd64ac66
Karthik Amaram <amaramkart09@gmail.com>
2025-07-06 11:20:16 -0500

    Created the logic and can fetch data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [data, setData] = useState(null)
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=12");
+        const json = await res.json();
+        const pokemonArray = json.results.map((pokemon) => {
+          const url = pokemon.url;
+          const parts = url.split("/");
+          const id = parts[parts.length - 2];
+          return {
+            name: pokemon.name,
+            id: id
+          }
+        })
+        setData(pokemonArray)
+      } catch (err) {
+        console.error("Fetch error:", err);
+      }
+    };
 
+    fetchData();
+  }, [])
 
 }
 
